339156d93c32529664a1ab0854e261a49d1242b9
Ram Chinthamsetty <ramchinthamsetty@Rams-MacBook-Air.local>
2019-11-30 18:29:37 +0530

    vs code settings sync

--- Python3/Advanced/argsinfunc.py
+++ Python3/Advanced/argsinfunc.py
@@ -1,16 +1,29 @@
+
+
 def func1(name_val, *args):
-    for val in args:
-        print(val, name_val)
+        """
+                Function to parse arguments given. *args are simple arguments.
+        """
+        for val in args:
+                # To print all values in the arguments.
+                print(val, name_val)
 
 def func2(name_val,**kwargs):
-    for key,val in kwargs.items():
-        print(key,val)
+        """
+                Function to process **kargs as key value pairs.
+                Args can be anything, but as key value pairs.
+                1. Application to constannt changing data types.
+        """
+        for key,val in kwargs.items():
+                print(key,val)
  
-        
-kwargs2 = {"arg3": 3, "arg2": "two","arg1":5}    
+ 
+ 
 func2("rama", key1="ram",key2="rk",key3="ramya")
-
-
 func1("rama", "python", "is","good", "to", "learn")
+# Passing more complex data as key value pairs.
+func2("Passing Dict", value1 = ["This", "is", "Somthing","Challenging"], value2= {"key1":"value1"})
+
+
 
     
