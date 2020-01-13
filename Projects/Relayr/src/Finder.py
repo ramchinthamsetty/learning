8f038741757b4cddefa0246e0ae98a43bdbbb697
Naga Sree Rama Krishna Chinthamsetty <N74Z@eurofins.com>
2020-01-13 17:35:59 +0530

    Adding Code to Finder Class

--- Projects/Relayr/src/Finder.py
+++ Projects/Relayr/src/Finder.py
@@ -1,11 +1,12 @@
 class Finder:
     
-    def __init__(self):
-        self.string_patterns = []
+    def __init__(self, Strings):
+        self.string_patterns = Strings
         
     def find_match(self, match_string):
         """
-        
+        match_string - Input value to match strings in the list
+        @return - List if there are match of strings irrespective of order of characters in match_string
         """
         
         return []
